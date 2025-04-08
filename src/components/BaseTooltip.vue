5f38c7342d6d745fbf3b44856e9c66ece57e203e
Nate Babyak <nate.babyak@outlook.com>
2025-04-07 22:04:04 -0400

    Refactor BaseTooltip component for improved visibility handling and structure; update tooltip display logic and styles

--- src/components/BaseTooltip.vue
+++ src/components/BaseTooltip.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-type Direction = 'north' | 'east' | 'south' | 'west'
+type Direction = 'north' | 'south' | 'east' | 'west'
 
 defineProps<{
   location: Direction
@@ -10,14 +10,23 @@ defineProps<{
 
 const visible = ref<Boolean>(false)
 
-const hide = () => (visible.value = false)
-const show = () => (visible.value = true)
+let timeout: ReturnType<typeof setTimeout> | null = null
+
+const show = () => {
+  timeout = setTimeout(() => (visible.value = true), 300)
+}
+
+const hide = () => {
+  if (timeout) clearTimeout(timeout)
+  visible.value = false
+}
 </script>
 
 <template>
   <div class="tooltip-wrapper" @mouseenter="show" @mouseleave="hide">
-    <div class="tooltip" v-if="visible" :class="location">
-      <span class="tooltip-text"> {{ text }}</span>
+    <slot></slot>
+    <div class="tooltip" v-show="visible" :class="location" @mouseenter="hide">
+      <span class="tooltip-text"> {{ text }} </span>
       <span class="tooltip-arrow" :class="location"></span>
     </div>
   </div>
@@ -29,8 +38,52 @@ const show = () => (visible.value = true)
 }
 
 .tooltip {
-  padding: 0.25rem;
+  background-color: var(--page-background);
+  border: 1px solid var(--senary-contrast);
+  border-radius: 9999px;
+  padding: 0.25rem 0.5rem 0.25rem 0.5rem;
   position: absolute;
   z-index: 1002;
+}
+
+.tooltip.north {
+}
+
+.tooltip.south {
+  left: -50%;
+  top: calc(100% + 0.75rem);
+}
+
+.tooltip.east {
+}
+
+.tooltip.west {
+}
+
+.tooltip-arrow {
+  -webkit-transform: rotate(45deg);
+  background-color: var(--page-background);
+  border: 1px solid var(--senary-contrast);
+  height: 0.7071rem;
+  position: absolute;
+  transform: rotate(45deg);
+  width: 0.7071rem;
+  z-index: 1003;
+}
+
+.tooltip-arrow.north {
+}
+
+.tooltip-arrow.south {
+  border-bottom: none;
+  border-right: none;
+  left: calc(50% - 0.44em);
+  top: -0.4rem;
+}
+
+.tooltip-arrow.east {
+}
+
+.tooltip-arrow.west {
 }
 </style>
